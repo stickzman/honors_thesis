f97e62cc38384cbc7fb4f1c7e710b9236f303f93
stickzman <Daniel.ahl1@marist.edu>
2017-09-15 12:45:48 -0400

    Added some more comments to unfrozenlake

--- unfrozenlake.py
+++ unfrozenlake.py
@@ -1,11 +1,20 @@
 import numpy
 
+discountRate = .97
+learnRate = .85
+totalEps = 1000
+
+#-------------------------------------------------------------
+#Recreate Frozen Lake w/o slipping function
+
 class Env:
+	#Representation of tiles in lake array
 	SAFE_TILE = 0
 	HOLE_TILE = 1
 	START_TILE = 3
 	GOAL_TILE = 4
 	
+	#Representation of moves in move array
 	UP = 0
 	DOWN = 1
 	LEFT = 2
@@ -36,21 +45,26 @@ class Env:
 		for i in range(12, 16):
 			self.moveMatrix[i][self.DOWN] = -1
 	
-	#Check if done
+	#Check if the player is on the Goal tile
 	def isWin(self):
 		return self.pIndex == 15
 		
+	#Check if player is in a hole
 	def isFallen(self):
 		return self.pIndex == 5 or self.pIndex == 7 or self.pIndex == 11 or self.pIndex == 12
 	
 	#Execute the action and advance one timestep
+	#Return the state, reward, and if the episode is done
 	def step(self, action):
 		reward = 0
 		done = False
 		newState = self.moveMatrix[self.pIndex][action]
-		if newState == -1:
+		if newState == -1: 
+			#If player attempted to move into a non-existent state,
+			#do not move player and return
 			return (self.pIndex, reward, done)
 		self.pIndex = newState
+		#Adjust the award according to current state
 		if self.isWin():
 			done = True
 			reward = 1
@@ -86,27 +100,21 @@ class Env:
 		
 #------------------------------------------------------------
 #Implement the same learning algorithm from frozenlake.py
-	
-y = .97 #Discount Rate
-learnRate = .85
-totalEps = 1000
-
-def updateQMat(q, reward, state, action, newState):
-	futureReward = max(q[newState][:])
-	q[state][action] = q[state][action] + learnRate * (reward + y * futureReward - q[state][action])
-	return
-
-
 
 prevSuccess = False
 firstSuccessEp = -1
 totalSuccessEps = 0
+	
+def updateQMat(q, reward, state, action, newState):
+	futureReward = max(q[newState][:]) #Maximum future reward from new state
+	q[state][action] = q[state][action] + learnRate * (reward + discountRate * futureReward - q[state][action])
+	return
 
 	
 env = Env()
 qMatrix = numpy.zeros((16, 4)) #Initialize qMatrix to 0s
 for e in range(totalEps):
-	observation = env.reset()
+	state = env.reset()
 	for t in range(1000):
 		#Create an array of random estimated rewards representing each action
 		#with the possible range of rewards decreasing with each episode
@@ -114,10 +122,10 @@ for e in range(totalEps):
 		#Choose either the action with max expected reward, or a random action
 		#according to randomActions array. With each episode, the random actions
 		#will become less chosen.
-		action = numpy.argmax(qMatrix[observation][:] + randomActions)
-		oldObservation = observation;
-		observation, reward, done = env.step(action)
-		updateQMat(qMatrix, reward, oldObservation, action, observation) #Update the Q-Matrix
+		action = numpy.argmax(qMatrix[state][:] + randomActions)
+		oldObservation = state;
+		state, reward, done = env.step(action)
+		updateQMat(qMatrix, reward, oldObservation, action, state) #Update the Q-Matrix
 		env.render()
 		if done:
 			if reward == 1:
