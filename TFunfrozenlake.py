844a524991e2e020e98e14870613b7f6c39af188
stickzman <Daniel.ahl1@marist.edu>
2017-09-21 15:45:29 -0400

    Added TensorFlow implementation to frozenlake and unfrozenlake problems

--- TFunfrozenlake.py
+++ TFunfrozenlake.py
@@ -1,19 +1,16 @@
-import numpy
-import random 
-
-discountRate = .97
-learnRate = .15
-totalEps = 1000
+import numpy as np
+import random
+import tensorflow as tf
 
 #-------------------------------------------------------------
 #Recreate Frozen Lake w/o slipping function
 
 class Env:
 	GOAL_REWARD = 1
-	HOLE_REWARD = -1 	
+	HOLE_REWARD = -1
 	DEFAULT_REWARD = 0
 	
-	SLIP_PERCENT = .2
+	SLIP_PERCENT = 0
 
 	#Representation of tiles in lake array
 	SAFE_TILE = 0
@@ -29,7 +26,7 @@ class Env:
 	
 	def __init__(self):
 		self.pIndex = 0 #Player index, also the current state
-		self.lakeArray = numpy.zeros(16) #Initialize lake to all safe tiles
+		self.lakeArray = np.zeros(16) #Initialize lake to all safe tiles
 		self.lakeArray[0] = self.START_TILE
 		self.lakeArray[15] = self.GOAL_TILE
 		self.lakeArray[5] = self.HOLE_TILE
@@ -111,50 +108,84 @@ class Env:
 		self.pIndex = 0
 		return self.pIndex
 		
-#------------------------------------------------------------
-#Implement the same learning algorithm from frozenlake.py
 
-prevSuccess = False
+
+tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1,16],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([16,4],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1,4],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+env = Env()
+
+# Set learning parameters
+y = .99
+e = 0.1
+num_episodes = 2000
+
+success = False
 firstSuccessEp = -1
 totalSuccessEps = 0
 lastFailedEp = -1
-	
-def updateQMat(q, reward, state, action, newState):
-	futureReward = max(q[newState][:]) #Maximum future reward from new state
-	q[state][action] = q[state][action] + learnRate * (reward + discountRate * futureReward - q[state][action])
-	return
 
-	
-env = Env()
-qMatrix = numpy.zeros((16, 4)) #Initialize qMatrix to 0s
-for e in range(totalEps):
-	state = env.reset()
-	for t in range(1000):
-		#Create an array of random estimated rewards representing each action
-		#with the possible range of rewards decreasing with each episode
-		randomActions = numpy.random.randn(1, 4)*(1/(e+1))
-		#Choose either the action with max expected reward, or a random action
-		#according to randomActions array. With each episode, the random actions
-		#will become less chosen.
-		action = numpy.argmax(qMatrix[state][:] + randomActions)
-		oldObservation = state;
-		state, reward, done = env.step(action)
-		updateQMat(qMatrix, reward, oldObservation, action, state) #Update the Q-Matrix
-		env.render()
-		if done:
-			if reward == env.GOAL_REWARD:
-				if prevSuccess == False:
-					#Record the first successful ep
-					prevSuccess = True
-					firstSuccessEp = e
-				totalSuccessEps += 1
-			else:
-				lastFailedEp = e
-			print("Episode finished after {} timesteps".format(t+1))
-			break
-			
+
+#create lists to contain total rewards and steps per episode
+jList = []
+rList = []
+with tf.Session() as sess:
+	sess.run(init)
+	for i in range(num_episodes):
+		#Reset environment and get first new observation
+		s = env.reset()
+		rAll = 0
+		d = False
+		j = 0
+		#The Q-Network
+		while j < 99:
+			j+=1
+			#Choose an action by greedily (with e chance of random action) from the Q-network
+			a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(16)[s:s+1]})
+			if np.random.rand(1) < e:
+				a[0] = random.randint(0, 3)
+			#Get new state and reward from environment
+			s1,r,d = env.step(a[0])
+			#Obtain the Q' values by feeding the new state through our network
+			Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(16)[s1:s1+1]})
+			#Obtain maxQ' and set our target value for chosen action.
+			maxQ1 = np.max(Q1)
+			targetQ = allQ
+			targetQ[0,a[0]] = r + y*maxQ1
+			#Train our network using target and predicted Q values
+			_,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(16)[s:s+1],nextQ:targetQ})
+			rAll += r
+			s = s1
+			env.render()
+			if d == True:
+				if r==1:
+					totalSuccessEps += 1
+					if success == False:
+						success = True
+						firstSuccessEp = i
+				else:
+					lastFailedEp = i
+					print("FAILED")
+				#Reduce chance of random action as we train the model.
+				print("Episode finished after " + str(j) + " timesteps")
+				#e = 1./((i/50) + 10)
+				e = 1/(i+1)
+				break
 print()
-print("Percentage of successful episodes: " + str((totalSuccessEps/totalEps) * 100) + "%")
+print("Percent of successful episodes: " + str((totalSuccessEps/num_episodes)*100) + "%")
 print()
 print("First successful episode: " + str(firstSuccessEp))
 print()
--- frozenlake.py
+++ frozenlake.py
@@ -1,9 +1,9 @@
-import numpy
 import gym
-from gym import wrappers
+
+import numpy 	
 
 y = .97 #Discount Rate
-learnRate = .85
+learnRate = .2
 totalEps = 1000
 
 def updateQMat(q, reward, state, action, newState):
@@ -11,11 +11,14 @@ def updateQMat(q, reward, state, action, newState):
 	q[state][action] = q[state][action] + learnRate * (reward + y * futureReward - q[state][action])
 	return
 
-
+success = False
+lastFailEp = -1
+firstSuccEp = -1
+successEps = 0
 
 env = gym.make('FrozenLake-v0')
 
-env = wrappers.Monitor(env, '/tmp/recording', force=True) #Records performance data
+#env = wrappers.Monitor(env, '/tmp/recording', force=True) #Records performance data
 
 qMatrix = numpy.zeros((env.observation_space.n, env.action_space.n)) #Initialize qMatrix to 0s
 
@@ -32,11 +35,26 @@ for i in range(totalEps):
 		action = numpy.argmax(qMatrix[observation][:] + randomActions)
 		oldObservation = observation;
 		observation, reward, done, info = env.step(action) #Perform the action
+		if done and reward == 0:
+			reward = -1 #Edit reward to negative in the case of falling in a hole
 		updateQMat(qMatrix, reward, oldObservation, action, observation) #Update the Q-Matrix
 		env.render()
 		if done:
+			if reward == 1:
+				successEps += 1
+				if success == False:
+					success = True
+					firstSuccEp = i
+			else:
+				lastFailEp = i
 			print("Episode finished after {} timesteps".format(t+1))
 			break
 
+print()
+print("Percentage of successful episodes: " + str(successEps/totalEps * 100) + "%")
+print()
+print("First successful episode: " + str(firstSuccEp))
+print()
+print("Last failed episode: " + str(lastFailEp))
 env.close()
 #gym.upload('/tmp/recording', api_key='sk_fVhBRLT7S7e4MoHswIH5wg') #Uploads performance data
