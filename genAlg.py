db6b77aeb9dcf118b0d742d26dd41fcbb14c11cc
stickzman <Daniel.ahl1@marist.edu>
2017-11-10 01:07:42 -0500

    Implemented genetic algorithm (w/o mutation) in genAlg.py

--- genAlg.py
+++ genAlg.py
@@ -4,20 +4,68 @@ import tensorflow.contrib.slim as slim
 import gym
 
 class Population:
-	def __init__(self, envName, sess, genSize):
+	def __init__(self, envName, sess, genSize, numParents, numGens, minW=0, maxW=1, crossoverProb=.90):
+		self.numGens = numGens
+		self.crossoverProb = crossoverProb
+		self.numParents = numParents//2*2
 		self.gen = []
 		self.sess = sess
 		for i in range(genSize):
-			self.gen.append(Indv(sess, gym.make(envName)))
+			self.gen.append(Indv(sess, gym.make(envName), minW=minW, maxW=maxW))
+			
+	def run(self):
+		for i in range(self.numGens):
+			self.runGen()
+			parents = self.__parentSelection()
+			self.__crossover(parents)
+			for agent in self.gen:
+				if agent.done:
+					if agent not in parents: agent.updateGenome()
+					agent.reset()
+		
 			
 	def runGen(self):
 		while self.__minOneAgentRunning():
 			for agent in self.gen:
 				agent.step()
 		fit = self.__getFitness()
-		print(fit)
+		print("Avg fitness: " + str(np.average(fit)),  "Best fitness: " + str(np.amax(fit)))
 		
-	
+	def __parentSelection(self):
+		probs = self.__getProbs()
+		parentProbs = np.random.choice(probs, self.numParents, False, probs)
+		parents = []
+		for parent in parentProbs:
+			parents.append(self.gen[np.argmax(probs == parent)])
+		return parents
+		
+	def __crossover(self, parents):
+		offspringGenes = []
+		for i in range(0, len(parents), 2):
+			p1 = parents[i]
+			p2 = parents[i+1]
+			g1 = p1.genome
+			g2 = p2.genome
+			newG = []
+			for gene1, gene2 in zip(g1, g2):
+				if np.random.rand() > .5:
+					newG.append(gene1)
+				else:
+					newG.append(gene2)
+			offspringGenes.append(newG)
+		weakIdxs = np.argsort(self.__getFitness)[:len(offspringGenes)]
+		for weakId, gene in zip(weakIdxs, offspringGenes):
+			self.gen[i].reset(gene)
+			
+			
+		
+	def __getProbs(self):
+		fitness = self.__getFitness()
+		total = sum(fitness)
+		probs = []
+		for fit in fitness:
+			probs.append(fit/total)
+		return probs
 	
 	def __getFitness(self):
 		fitness = []
@@ -30,7 +78,9 @@ class Population:
 		for indv in self.gen:
 			if indv.done == False: res = True
 		return res
-	
+
+
+
 class Indv:	
 	def __init__(self, sess, env, minW=0, maxW=1, genome=None):
 		self.env = env
@@ -48,11 +98,11 @@ class Indv:
 			a_dist = self.sess.run(output,feed_dict)
 			
 			#Stochastic Selection
-			#a = np.random.choice(a_dist[0],p=a_dist[0])
-			#a = np.argmax(a_dist == a)
+			a = np.random.choice(a_dist[0],p=a_dist[0])
+			a = np.argmax(a_dist == a)
 			
 			#Deterministic Selection
-			a = np.argmax(a_dist)
+			#a = np.argmax(a_dist)
 			
 			self.input, r, self.done, _ = self.env.step(a)
 			self.fitness += r
@@ -115,16 +165,5 @@ sess = tf.Session()
 sess.run(init)
 
 
-pop = Population('CartPole-v0', sess, 5)
-pop.runGen()
-'''
-agent1 = Indv(sess, gym.make('CartPole-v0'))
-
-agent2 = Indv(sess, gym.make('CartPole-v0'))
-
-while not agent1.done or not agent2.done:
-	agent1.step()
-	agent2.step()
-print(agent1.fitness)
-print(agent2.fitness)
-'''
+pop = Population('CartPole-v0', sess, genSize=50, numParents=25, numGens=40, maxW=10)
+pop.run()
